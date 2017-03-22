d3ceb28172c610066be76b288e9510f1ecd4cdf8
C Crow <cgcrow@email.wm.edu>
2017-03-22 17:05:55 +0000

    Added avatar column to Profiles table

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,8 @@ class Profile < ActiveRecord::Base
   # Assications in Active Record can be found on the rails guide for 
   # Active Record Associations
   belongs_to :user
+  has_attached_file :avatar,
+    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+    :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
